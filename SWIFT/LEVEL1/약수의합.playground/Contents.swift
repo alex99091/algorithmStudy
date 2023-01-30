e30718b2fe9952774106f168ef869f028a742518
alex99091 <bkk99091@gmail.com>
2023-01-30 17:05:29 +0900

    01/30 - level 1: 3

--- SWIFT/LEVEL1/약수의합.playground/Contents.swift
+++ SWIFT/LEVEL1/약수의합.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
+//
+//문제 설명
+//정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+//
+//제한 사항
+//n은 0 이상 3000이하인 정수입니다.
 
-var greeting = "Hello, playground"
+func solution(_ n:Int) -> Int {
+    var answer = 0
+    if n == 0 {
+        answer == 0
+    } else {
+        for i in 1...n {
+            answer += n % i == 0 ? i : 0
+        }
+    }
+    return answer
+}
+
+func solution2(_ n:Int) -> Int {
+    guard n != 0 else {
+        return 0
+    }
+    return Array(1...n).filter{ n % 0 == 0}.reduce(0, +)
+}
--- SWIFT/LEVEL1/평균구하기.playground/Contents.swift
+++ SWIFT/LEVEL1/평균구하기.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//문제 설명
+//정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+func solution(_ arr:[Int]) -> Double {
+    var answer = 0
+    for i in arr {
+        answer += i
+    }
+    return  Double(answer) / Double(arr.count)
+}
+
+func solution2(_ arr:[Int]) -> Double {
+    return  Double(arr.reduce(0,+)/Double(arr.count))
+}
